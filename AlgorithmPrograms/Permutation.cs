f9ea7e12d456d99a64943afca3409f88f52700cc
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-14 17:14:20 +0530

    [Soubarnika]Printing all permutations of string

---
+++ AlgorithmPrograms/Permutation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    class Permutation
+    {
+        public void Permute(string str, int start, int end)
+        {
+            //if start and end point equals then the new string is formed
+            if (start == end)
+            {
+                Console.WriteLine(str);
+            }
+            //else repeat the below process
+            else
+            {
+                //iterate using for loop
+                for (int i = start; i <= end; i++)
+                {
+                    //swap the start position with ith position
+                    str = Swap(str, start, i);
+                    //call permutate method with start as next position
+                    Permute(str, start + 1, end);
+                    //again swap the string with new start and ith position
+                    str = Swap(str, start, i);
+                }
+            }
+        }
+        public string Swap(string value, int i, int j)
+        {
+            //convert the string to char array
+            char[] charArray = value.ToCharArray();
+            char temp;
+            //swap the item at two position
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string str = new string(charArray);
+            //return the string
+            return str;
+        }
+    }
+}
+
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -7,6 +7,11 @@ namespace AlgorithmPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Implementation of Algorithm Programs ");
+            Console.WriteLine("1.Permutation of strings");
+            Permutation permutation = new Permutation();
+            String str = "ABC";
+            int n = str.Length;
+            permutation.Permute(str, 0, n - 1);
         }
     }
 }
