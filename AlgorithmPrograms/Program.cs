74b416700fccdb75013bbb467282f32ea9472e50
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-14 17:21:55 +0530

    [Soubarnika]P1-Printing all permutations of string

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -7,11 +7,26 @@ namespace AlgorithmPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Implementation of Algorithm Programs ");
-            Console.WriteLine("1.Permutation of strings");
-            Permutation permutation = new Permutation();
-            String str = "ABC";
-            int n = str.Length;
-            permutation.Permute(str, 0, n - 1);
+            Console.WriteLine("1.Permutation of string");
+            Console.WriteLine("2.Exit");
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Permutation permutation = new Permutation();
+                    Console.WriteLine("Permuations of given string are:");
+                    String str = "ABC";
+                    int n = str.Length;
+                    permutation.Permute(str, 0, n - 1);
+                    break;
+
+               
+
+                case 2:
+                    break;
+            }
+            Console.Read();
         }
     }
 }
